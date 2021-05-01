88f25be11490f0f3d22ad325ea9aaea5569b5997
Jason <jasonpsmith180@gmail.com>
2021-05-01 12:18:54 -0600

    add post model

---
+++ routes/api/post-routes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+const sequelize = require('../../config/connection');
+
+// Get all posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        // query config
+        order: [['created_at', 'DESC']],
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        // include: [
+        //     {
+        //         model: Comment,
+        //         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        //         include: {
+        //             model: User,
+        //             attributes: ['username']
+        //         }
+        //     },
+        //     {
+        //         model: User,
+        //         attributes: ['username']
+        //     }
+        // ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
