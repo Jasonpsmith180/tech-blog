e7c57e6db22f0c27934937417c71c8063897c4cc
Jason <jasonpsmith180@gmail.com>
2021-05-02 20:30:05 -0600

    add single post and comment functions

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -12,22 +12,23 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     // check the session
-    // if (req.session)
+    if (req.session) {
         Comment.create({
             comment_text: req.body.comment_text,
             post_id: req.body.post_id,
-            user_id: req.body.user_id
+            user_id: req.session.user_id
         })
         .then(dbCommentData => res.json(dbCommentData))
         .catch(err => {
             console.log(err);
             res.status(400).json(err);
         });
+    }    
 });
 
 router.put('/:id', (req, res) => {
     // check the session
-    // if (req.session)
+    if (req.session) {
         Comment.update(
             {
                 comment_text: req.body.comment_text
@@ -49,6 +50,7 @@ router.put('/:id', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+    }    
 });
 
 router.delete('/:id', (req, res) => {
